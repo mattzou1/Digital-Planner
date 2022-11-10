046e9f3e3bd6d59e04e82d92915e0d2460f5566d
dlybeck <dlybeck383@gmail.com>
2022-11-10 13:30:30 -0800

    added comments

--- public/js/copyToDo.js
+++ public/js/copyToDo.js
@@ -83,8 +83,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+    //check and see if "holder" boxes are empty and set make them droppaple if they are
     function determineDraggable(){
         let holders = document.body.querySelectorAll(".holder");
+        //check each holder
         for(let i = 0; i < holders.length; i++){
             if(holders[i].hasChildNodes()){
                 holders[i].setAttribute("ondragover", "");
