918b0ee1cc5921cd4641320151314ba919956584
sve-solo <ssolodilov@pugetsound.edu>
2022-11-19 18:14:42 -0800

    Deleted unnecessary comment lines

--- public/js/AddAssignmentEventData.js
+++ public/js/AddAssignmentEventData.js
@@ -1,10 +1,6 @@
 //code to add event and assignment objects to fields in a user object and populate the ToDoList
 import {User,user1,Assignment, Event, weekdays} from "./classes.js";
 
-//let classes = require("../js/classes.js");
-//create user1
-//let user1 = new classes.User(("bob","123","9:30", "13:00"));
-
 window.addEventListener("DOMContentLoaded", registerSubmitButtons);
 
 let user = user1;
