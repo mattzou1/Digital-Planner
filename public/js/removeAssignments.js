4539d428ac77d6bbd12c6d9cf4b36f0ca8c5b294
dlybeck <dlybeck383@gmail.com>
2022-11-19 17:56:38 -0800

    commented out all shortcut stuff

--- public/js/removeAssignments.js
+++ public/js/removeAssignments.js
@@ -6,17 +6,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
         if(event.target.id == "removeButton"){
             //remove button pushed
             let targetElement = event.target
-            console.log("Button is " + targetElement)
-            console.log("Button parent is " + targetElement.parentElement)
-            console.log("button parent parent is " + targetElement.parentElement.parentElement)
             let holder = targetElement.parentElement.parentElement;
             let children = holder.children
             for(let i = 0; i < children.length; i++){
-                console.log("child is " + children[i].id)
-                console.log("child display type is: " + children[i].style.display)
                 //if the item assignment is visible only remove that one (not the one hidden in the same spot)
                 if(children[i].style.display == "block" || children[i].style.display == ""){
-                    console.log("Display is block... Removing")
                     children[i].remove()
                 }
             }
