52ccd6f68cee59eb992931df54e5623ffc8709d7
sve-solo <ssolodilov@pugetsound.edu>
2022-11-21 15:03:41 -0800

    Changed times to military time

--- public/js/increments.js
+++ public/js/increments.js
@@ -1,7 +1,15 @@
 
 //function that fills dropdown menu with times in 30 minute increments
 export function increments(select){
-    for(let i = 1; i < 13; i++){
+    for(let i = 1; i < 25; i++){
+        //adds "24:00" to dropdown menu
+        if(i == 24){
+            let temp = document.createElement("option")
+            temp.innerHTML = i + ":00";
+            temp.setAttribute("value", i + ":00")
+            select.appendChild(temp);
+            break;
+        }
         //create two option elements
         let newOption = document.createElement("option");
         let newOption2 = document.createElement("option");
