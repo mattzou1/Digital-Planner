ae0470b3b64e03abb0a68cc9a32715224fb168c0
dlybeck <dlybeck383@gmail.com>
2022-11-28 10:23:37 -0800

    added remove assignment method

--- public/js/removeAssignments.js
+++ public/js/removeAssignments.js
@@ -27,9 +27,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
                         newCell.setAttribute("ondragover", "allowDrop(event)");
                     }
                 }
-
-
-
             }
 
             if(assignment.style.display != "none" || assignment.className == "assignment"){
@@ -37,6 +34,38 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         }
     });
+
+    function removeAssignment (assignment){
+        //to make sure all removeButtons are detected it detects all clicks and continues if it is on #removeButton
+        if(assignment.className == "assignment"){
+            let table = document.body.querySelector("table");
+            let assignment = targetElement.parentElement;
+            let cell = assignment.parentElement;
+
+
+            if(assignment.id.includes("Copy")){
+                //shrink back to 1 cell to then replace missing ones before deletion
+                cell.rowSpan = 1;
+
+                for (let row of table.rows) {
+                    if(row.cells[1] == undefined){
+                        //insert at the end of the row
+                        let newCell = row.insertCell(-1)
+
+                        //add cell attributes
+                        newCell.id = "calendarBox" + row.cells[0].innerHTML;
+                        newCell.setAttribute("class", "holder");
+                        newCell.setAttribute("ondrop", "drop(event)");
+                        newCell.setAttribute("ondragover", "allowDrop(event)");
+                    }
+                }
+            }
+
+            if(assignment.style.display != "none" || assignment.className == "assignment"){
+                assignment.remove()
+            }
+        }
+    }
 })
 
 import {User,user1,Assignment, Event, weekdays} from "./classes.js";
