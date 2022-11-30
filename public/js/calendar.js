79fe815602f3e56d68cf13f083ff53eaec39f19f
dlybeck <dlybeck383@gmail.com>
2022-11-29 21:44:29 -0800

    changed color slightly just becasue + added a border

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -10,11 +10,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const eventButton = document.body.querySelector("#eventButton");
 
 
-    //set assignments as "clicked" by default
-    assignmentButton.style.backgroundColor = "#D47272";
-    shortcutButton.style.backgroundColor = "lightcoral";
-
-
     //event listeners for button clicks
     shortcutButton.addEventListener("click", ShortcutTab);
     assignmentButton.addEventListener("click", AssignmentTab);
@@ -42,27 +37,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
-    //Change to Assignments tab
-    function AssignmentTab(){
-        //select all the assignments and shortcuts
-        const shortcuts = document.body.querySelectorAll("#shortcut");
-        const assignments = document.body.querySelectorAll("#assignment");
- 
-        //change color of tab
-        assignmentButton.style.backgroundColor = "#D47272";
-        shortcutButton.style.backgroundColor = "lightcoral";
-
-
-        //hide shortcuts
-        for(let i = 0; i < shortcuts.length; i++){
-            shortcuts[i].style.display = "none";
-        }
-
-        //show assignments
-        for(let i = 0; i < assignments.length; i++){
-            assignments[i].style.display = "block";
-        }
-    }
 
     //alert that an assignment was added on click
     function addAssignment(){
