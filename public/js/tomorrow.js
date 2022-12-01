902168cf19491f856eb3e1e0f4882e1c497149c5
sve-solo <ssolodilov@pugetsound.edu>
2022-11-30 17:49:37 -0800

    Fixed bug in calendar dates

--- public/js/tomorrow.js
+++ public/js/tomorrow.js
@@ -18,52 +18,52 @@ function handler(){ //event handler for after DOM is loaded
 
     let today = new Date();
     let num = today.getDay();
+    // let storeToday = today.getDay();
+    // let storeToday2 = today;
+    ///console.log("storeToday2" + storeToday2);
 
     //if tomorrow button is clicked
     tmrButton.addEventListener("click", function() {
         //get day of week
         
-        //console.log(user1.getTomorrow());
-
-        //console.log(weekdays[num]);
-
-        if(num > 6){
+        if(num > 5){
              num = -1;
         }
 
-        //update date to tomorrow
-        date.innerHTML = weekdays[nums[num + 1]];
-        console.log(weekdays[nums[num + 1]]);
-
         num = num + 1;
-    
+
+        //update date to tomorrow
+        date.innerHTML = weekdays[nums[num]];
+        console.log(weekdays[nums[num]]);
+
+        console.log("num: " + num);
+
         let newDate = today.setDate(today.getDate() + 1);
         today = new Date(newDate);
-        console.log(today);
+        //change today to be the next day 
+        //today.setDate(today.getDate() + 1);
 
         let day = today.getDate();
+        //console.log("day: " + day);
         let month = today.toLocaleString("default", { month: "long"});
         let year = today.getFullYear();
+
+        //console.log("month: " + day);
+        //console.log("year: " + day);
         
         calheader.innerHTML = `${month} ${day}, ${year}`;
 
-        console.log("num: " + num);
+        ///console.log("today: " + today);
+        //console.log("num: " + num);
         //clear all events from calendar
         //keep repeating events
-        
+        ///console.log("num: " + num);
     });
 
     //get the back button
     let backButton = document.querySelector("#backButton");
 
     backButton.addEventListener("click", function() {
-        // if(num < 7 && num > 0){
-        //     num = num - 1;
-        //     date.innerHTML = weekdays[num];
-        //     console.log("num: " + num);
-        //     console.log(weekdays[num]);
-        // }
-
         //get nums
         //subtract num to get the previous day
 
@@ -71,14 +71,15 @@ function handler(){ //event handler for after DOM is loaded
             num = 7;
         }
         num = num - 1;
-        console.log("num: " + num);
-        console.log(weekdays[nums[num]]);
+        ///console.log("num: " + num);
+        ///console.log(weekdays[nums[num]]);
         date.innerHTML = weekdays[nums[num]];
         
 
         let newDate = today.setDate(today.getDate() - 1);
         today = new Date(newDate);
-        console.log(today);
+        //today.setDate(today.getDate() - 1);
+        ///console.log(today);
 
         let day = today.getDate();
         let month = today.toLocaleString("default", { month: "long"});
@@ -91,15 +92,30 @@ function handler(){ //event handler for after DOM is loaded
         
     });
 
-    let todayButton = document.querySelector("#todayButton");
-    //get button for 'today'
-    todayButton.addEventListener("click", function() {
-        //update date to today
-        date.innerHTML = user1.getDayOfWeek();
+    // let todayButton = document.querySelector("#todayButton");
+    // //get button for 'today'
+    // todayButton.addEventListener("click", function() {
+
+    //     ///console.log("storeToday2: " + storeToday2);
+
+    //     //update date to today
+    //     date.innerHTML = user1.getDayOfWeek();
         
-        //update cal header
-        calheader.innerHTML = user1.getFormattedDate();
-    });
+    //     //update cal header
+    //     calheader.innerHTML = user1.getFormattedDate();
+
+    //     num = storeToday; //rest num to the current day of week number
+    //     ///console.log("should be 3: " + num);
+
+    //     //today.setDate(storeToday);
+    //     //console.log("today.getDate: " + today.getDate());
+
+    //     //let newDate = today.setDate(storeToday2);
+    //     today = new Date(storeToday2 - 1);
+    //     ///console.log("today: " + today);
+
+
+    // });
 
 
 }
