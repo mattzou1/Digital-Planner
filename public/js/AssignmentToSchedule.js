2c24249efda5e45a6bc3bbf08423c083da3454dc
dlybeck <dlybeck383@gmail.com>
2022-12-03 15:20:36 -0800

    merge

---
+++ public/js/AssignmentToSchedule.js
@@ -0,0 +1,25 @@
+import {getUser,getStopTime} from "./classes.js";
+let user = getUser();
+
+window.addEventListener("DOMContentLoaded", assToSchedule);
+
+
+function assToSchedule(){
+    let table = document.querySelector("table");
+    table.addEventListener('mouseover', (event) => {
+        let targetElement = event.target
+        if(targetElement.className == "assignment"){
+            let cell = targetElement.parentNode
+            let row = cell.parentNode;
+            let startTime = row.cells[0].innerHTML
+            let length = cell.rowSpan * 30
+            let assignmentid = targetElement.id; 
+            let index = assignmentid.substring(10,11);
+            user.addElement(startTime, getStopTime(startTime, length), user.assignments[index]);
+            console.log(user.assignments);
+            console.log(user.schedule);
+
+        }
+    });
+}
+
