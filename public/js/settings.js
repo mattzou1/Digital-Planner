583e4a2b6e968d84d5749b97f25b5b9e404f9596
sve-solo <ssolodilov@pugetsound.edu>
2022-12-07 19:24:59 -0800

    Added semi-colons/parentheses

--- public/js/settings.js
+++ public/js/settings.js
@@ -100,11 +100,10 @@ import {User} from "./classes.js";
                 // console.log("here is hopefully changed theUser...");
                 // console.log(theUser);
                 // //change the username to whatever
-            }
-        });
+            //}
+       // });
     }
 
 
-
-
-
+    });
+}
